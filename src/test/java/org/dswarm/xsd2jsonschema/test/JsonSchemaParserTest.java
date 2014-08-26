ebff142e1235e3ffc368308f7a2817359f74a774
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-08-26 15:13:21 +0200

    upgraded depdency packages versions

--- src/test/java/org/dswarm/xsd2jsonschema/test/JsonSchemaParserTest.java
+++ src/test/java/org/dswarm/xsd2jsonschema/test/JsonSchemaParserTest.java
@@ -1,20 +1,17 @@
 package org.dswarm.xsd2jsonschema.test;
 
-import java.io.InputStream;
-import java.net.URL;
-
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Charsets;
-import com.google.common.io.InputSupplier;
+import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
-
 import org.dswarm.xsd2jsonschema.JsonSchemaParser;
 import org.dswarm.xsd2jsonschema.model.JSRoot;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
 
 public class JsonSchemaParserTest {
 
@@ -27,9 +24,9 @@ public class JsonSchemaParserTest {
 		final JsonSchemaParser schemaParser = new JsonSchemaParser();
 
 		final URL resourceURL = Resources.getResource("mabxml-1.xsd");
-		InputSupplier<InputStream> bla = Resources.newInputStreamSupplier(resourceURL);
+		final ByteSource byteSource = Resources.asByteSource(resourceURL);
 
-		schemaParser.parse(bla.getInput());
+		schemaParser.parse(byteSource.openStream());
 		final JSRoot root = schemaParser.apply("bla");
 
 		final ObjectNode json = root.toJson(objectMapper);
